06c11bba84d89eaed41ff7a7701883b91f693f30
goutamkumar120 <goutamkumar120@gmail.com>
2025-07-31 19:34:55 +0530

    First commit

---
+++ src/main/java/com/goutam/SampleSingbootProjectApplication.java
@@ -0,0 +1,16 @@
+package com.goutam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSingbootProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SampleSingbootProjectApplication.class, args);
+
+		System.out.println("Welcome to Sample Spring-boot Project.");
+	}
+
+}
